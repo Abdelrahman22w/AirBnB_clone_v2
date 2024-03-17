c0747248e22d30b17ae2901d8c6d9cbf8928bcce
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2024-03-17 16:25:07 +0200

    sds

--- models/base_model.py
+++ models/base_model.py
@@ -39,6 +39,13 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    def delete(self):
+        """public instance method to delete the current instance
+        from the storage"""
+        from models import storage
+        storage.delete(self)
