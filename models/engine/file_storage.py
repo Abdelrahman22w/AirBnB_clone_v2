c9c19670513bf4c8bc2867d82ef200f3d03b3ec9
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2024-04-29 22:09:06 +0300

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,6 @@ from models.user import User
 
 class FileStorage:
     """Represent an abstracted storage engine.
-
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
@@ -23,18 +22,18 @@ class FileStorage:
 
     def all(self, cls=None):
         """Return a dictionary of instantiated objects in __objects.
-
         If a cls is specified, returns a dictionary of objects of that type.
         Otherwise, returns the __objects dictionary.
         """
-        if cls:
-            if isinstance(cls,str):
-                cls = globals().get(cls)
-            if cls and issubclass(cls, BaseModel):
-                cls_dict = {k: v for k,
-                            v in self.__objects.items()if isinstance(v, cls)}
-                return cls_dict
-        return FileStorage.__objects
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id."""
@@ -59,18 +58,10 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Delete a given object from __objects, if it exists."""
-        if obj is None:
-            return
-        obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
-
         try:
-            del FileStorage.__objects[obj_to_del]
-        except AttributeError:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
             pass
-        except KeyboardInterrupt:
-            pass
-
-        
 
     def close(self):
         """Call the reload method."""
